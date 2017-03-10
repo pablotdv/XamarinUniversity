8f2a98c6039b063292b6c9aa1b7287f34e7f457d
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-03-09 22:37:21 -0300

    Xamarin University - Exercise 3

--- TipCalculator/TipCalculator/MainActivity.cs
+++ TipCalculator/TipCalculator/MainActivity.cs
@@ -11,12 +11,32 @@ namespace TipCalculator
     [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView(Resource.Layout.Main);            
+            SetContentView(Resource.Layout.Main);
+
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += OnCalculateClick;
+        }
+
+        private void OnCalculateClick(object sender, EventArgs e)
+        {
+            string text = inputBill.Text;
+            var bill = double.Parse(text);
+            var tip = bill * 0.15;
+            var total = bill + tip;
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = total.ToString();
         }
     }
 }
